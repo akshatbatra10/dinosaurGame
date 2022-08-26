dbdf6fa23858f8e171a7a7c14a6188db70f52e0c
akshatbatra10 <62565788+akshatbatra10@users.noreply.github.com>
2022-08-26 16:41:45 +0530

    added obstacle animation

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const dino = document.getElementById("dino");
 
 const jump = () => {
-  dino.classList.add("jump");
+  if (dino.classList != "jump") {
+    dino.classList.add("jump");
+    setTimeout(() => {
+      dino.classList.remove("jump");
+    }, 300);
+  }
 };
 
 document.addEventListener("keydown", (e) => {
