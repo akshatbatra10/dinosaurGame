055fe5099bd767e048befd2f9225d7d6cdb20387
akshatbatra10 <62565788+akshatbatra10@users.noreply.github.com>
2022-08-26 16:55:15 +0530

    added function to check collision between obstacle and dinosaur

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
 
 const jump = () => {
   if (dino.classList != "jump") {
@@ -8,6 +9,16 @@ const jump = () => {
     }, 300);
   }
 };
+
+const isAlive = setInterval(() => {
+  let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+  let cactusLeft = parseInt(
+    window.getComputedStyle(cactus).getPropertyValue("left")
+  );
+  if (dinoTop >= 140 && cactusLeft < 50 && cactusLeft > 0) {
+    alert("Game Over");
+  }
+}, 10);
 
 document.addEventListener("keydown", (e) => {
   jump();
